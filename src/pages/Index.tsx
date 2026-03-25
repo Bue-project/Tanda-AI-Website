1398b8ea7e808ff9511e7dee59be63a34446d300
Bue-project <bue07092205@gmail.com>
2026-03-25 12:57:26 +0000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,14 +3,9 @@ import Hero from '@/components/Hero';
 import WhatWeDo from '@/components/WhatWeDo';
 import Benefits from '@/components/Benefits';
 import Services from '@/components/Services';
-import Integrations from '@/components/Integrations';
 import Features from '@/components/Features';
-import Process from '@/components/Process';
-import Pricing from '@/components/Pricing';
 import Comparison from '@/components/Comparison';
 import FAQ from '@/components/FAQ';
-import Reviews from '@/components/Reviews';
-import Team from '@/components/Team';
 import CTA from '@/components/CTA';
 import Footer from '@/components/Footer';
 
@@ -22,14 +17,9 @@ export default function Index() {
       <WhatWeDo />
       <Benefits />
       <Services />
-      <Integrations />
       <Features />
-      <Process />
-      <Pricing />
       <Comparison />
       <FAQ />
-      <Reviews />
-      <Team />
       <CTA />
       <Footer />
     </div>
